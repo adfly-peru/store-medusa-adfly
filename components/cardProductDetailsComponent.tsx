9b9039702e330ab4c2c4d555a3b4b1fd43d59661
juan4056 <juan.vargasg2@gmail.com>
2022-12-07 03:07:40 -0500

    style:change style for card details component

--- components/cardProductDetailsComponent.tsx
+++ components/cardProductDetailsComponent.tsx
@@ -1,4 +1,4 @@
-import { createStyles, Text, Card, Group, CardSection, Badge, Image, Grid, ActionIcon, NumberInput, Button, NumberInputHandlers } from '@mantine/core';
+import { createStyles, Text, Card, Group, CardSection, Badge, Image, Grid, ActionIcon, NumberInput, Button, NumberInputHandlers, Stack, SimpleGrid, Center, Flex, Title, Divider, Space, Breadcrumbs, Anchor } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { useRef, useState } from 'react';
 import Product from '../interfaces/productInterface';
@@ -12,12 +12,11 @@ const useStyles = createStyles((theme) => ({
 
 export function CardProductDetails({ product } : {product: Product}) {
   const { classes, theme } = useStyles();
+  const [ imgIdx, setIdx ] = useState(0);
 
   // Calculo de precio final
   let finalPrice = product.originalPrice - (product.originalPrice * product.discount / 100);
   let ahorro = product.originalPrice - product.finalPrice;
-
-
 
   const [showBuy, setShowBuy] = useState(false);
   const [value, setValue] = useState(0);
@@ -32,91 +31,89 @@ export function CardProductDetails({ product } : {product: Product}) {
 
 
   return (
-    <Card withBorder p="xl" radius="md" className={classes.card}>
-
-      <CardSection>
-        <Group position="apart">
-          <Badge m={5} color="red">-{product.discount}%</Badge>
-        </Group>
-      </CardSection>
-      <CardSection>
-        <Grid>
-          <Grid.Col xs={6}>
-            <Image src={product.imgUrl[0]} alt={product.imgUrl[0]} height={200} fit="contain" withPlaceholder/>
-            <Grid>
-              <Grid.Col xs={3}>
-                <Image src={product.imgUrl[0]} alt={product.imgUrl[0]} height={100} fit="contain" withPlaceholder/>
-              </Grid.Col>
-              <Grid.Col xs={3}>
-                <Image src={product.imgUrl[0]} alt={product.imgUrl[0]} height={100} fit="contain" withPlaceholder/>
-              </Grid.Col>
-              <Grid.Col xs={3}>
-                <Image src={product.imgUrl[0]} alt={product.imgUrl[0]} height={100} fit="contain" withPlaceholder/>
-              </Grid.Col>
-            </Grid>
-
-          </Grid.Col>
-
-          <Grid.Col xs={6}>
-          <Text mt={5}>{product.brand}</Text>
-        <Text fz="xl" fw={700}>{product.name}</Text>
-        <Group>
-          <Text fz="sm" td="line-through">S/. {product.originalPrice}</Text>
-          <Text c="red">S/. {finalPrice}</Text>
-        </Group>
-        <Text fz="sm">(o {product.stars} estrellas)</Text>
-        <Text fz="sm" c="red">Ahorro estimado S/. {ahorro}</Text>
-        <Text fz="sm">Vendido por Partner</Text>
-        <Text fz="sm">Stock: {product.details.stock} Unidad(es)</Text>
-        <Text fz="sm">Fecha de Vencimiento: {product.details.expirationDate}</Text>
-        
-
-
-          <Group
-            spacing={5}
-            position="center"
-            style={{ marginTop: 15 }}
-          >
-          <ActionIcon size={36} variant="default" onClick={() => handlers.current?.decrement()}>
-            –
-          </ActionIcon>
-    
-          <NumberInput
-            hideControls
-            value={value}
-            onChange={(val: number) => setValue(val)}
-            handlersRef={handlers}
-            max={10}
-            min={0}
-            step={1}
-            styles={{ input: { width: 70, textAlign: 'center' } }}
-          />
-    
-          <ActionIcon size={36} variant="default" onClick={() => handlers.current?.increment()}>
-            +
-          </ActionIcon>
-        </Group>
-          <Button
-            variant="light"
-            color="blue"
-            fullWidth
-            mt="md"
-            radius="md"
-            onClick={() => {setShowBuy(true); setValue(1)}}
-          >
-            Agregar
-          </Button>
-        
-          </Grid.Col>
-        </Grid>
+    <Card withBorder py="xl" px="xl" radius="md" className={classes.card}>
+      <CardSection inheritPadding withBorder py="xs">
+        <Breadcrumbs separator="→">
+          <Anchor fw={500} color="dark" href={'/home'}>
+            Inicio
+          </Anchor>
+          <Anchor fw={500} color="dark" href={'/home'}>
+            {product.brand}
+          </Anchor>
+          <Text>{product.name}</Text>
+        </Breadcrumbs>
       </CardSection>
       
-      <CardSection>
+      <CardSection inheritPadding py="xl">
+        <Group align="start" position="apart" grow>
+          <Stack spacing="xs">
+            <div style={{ width: 70 }}>
+              <Badge size="lg" color="red" radius="xs" variant="filled" fullWidth>-{product.discount}%</Badge>
+            </div>
+            <Space h="lg"/>
+            <Image src={product.imgUrl[imgIdx]} alt={product.imgUrl[imgIdx]} height={200} fit="contain" withPlaceholder/>
+            <Flex>
+              {
+                product.imgUrl.map( ( img, idx ) =>
+                  <Image onClick={()=>setIdx(idx)} key={idx} src={img} alt={img} height={100} fit="contain" withPlaceholder/>
+                )
+              }
+            </Flex>
+          </Stack>
+          <Stack spacing="xs">
+            <Title order={1}>{product.name}</Title>
+            <Text fw={500}>Marca: {product.brand}</Text>
+            <Group>
+              <Text c="dimmed" fw={100} fz="sm" td="line-through">S/. {product.originalPrice}</Text>
+              <Text c="red" fw={700}>S/. {finalPrice}</Text>
+            </Group>
+            <Text fw={450}>o {product.stars} estrellas ⭐</Text>
+            <Text fz="xs">(Ahorro estimado S/. {ahorro})</Text>
+            <Divider my="sm" />
+            <Text>Vendido por <Text span fw={500} inherit>Partner</Text></Text>
+            <Divider my="sm" />
+            <Text fz="xs">Stock: {product.details.stock} Unidad(es)</Text>
+            <Text fz="xs">Fecha de Vencimiento: {product.details.expirationDate}</Text>
+            <Group
+              spacing={5}
+            >
+              <ActionIcon size={36} variant="default" onClick={() => handlers.current?.decrement()}>
+                –
+              </ActionIcon>
+              <NumberInput
+                hideControls
+                value={value}
+                onChange={(val: number) => setValue(val)}
+                handlersRef={handlers}
+                max={10}
+                min={0}
+                step={1}
+                styles={{ input: { width: 70, textAlign: 'center' } }}
+              />
+              <ActionIcon size={36} variant="default" onClick={() => handlers.current?.increment()}>
+                +
+              </ActionIcon>
+              <Space />
+              <Text fz="xs" c="dimmed">Máximo: x unidades este mes.</Text>
+            </Group>
+            <Button
+              variant="light"
+              color="blue"
+              fullWidth
+              mt="md"
+              radius="md"
+              onClick={() => {setShowBuy(true);}}
+            >
+              Agregar
+            </Button>
+          </Stack>
+        </Group>
+      </CardSection>
+      
+      <CardSection inheritPadding withBorder py="xs">
         <Text>Detalles del Producto</Text>
         <Text>{product.details.details}</Text>
-
       </CardSection>
-
     </Card>
   );
 }
