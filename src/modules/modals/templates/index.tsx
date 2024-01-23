aa3f2f32256baf0a27784030d56c5b1e34cb3025
juan4056 <juan.vargas@utec.edu.pe>
2024-01-23 14:53:55 -0500

    feat: add time to modal

--- src/modules/modals/templates/index.tsx
+++ src/modules/modals/templates/index.tsx
@@ -12,8 +12,22 @@ const CollaboratorModals: React.FC<{ children?: React.ReactNode }> = ({
     if (!collaborator) return;
     if (!collaborator.emailVerify || !collaborator.changePassword) return;
 
+    const lastOpened = localStorage.getItem("surveyModalTime");
+    const now = new Date().getTime();
+
+    // const fiveHoursInMs = 5 * 60 * 60 * 1000;
+    const fiveHoursInMs = 5 * 60 * 1000;
+
+    if (lastOpened && now - parseInt(lastOpened) < fiveHoursInMs) {
+      return;
+    }
+
+    localStorage.setItem("surveyModalTime", now.toString());
+
     modals.open({
       withCloseButton: false,
+      closeOnClickOutside: false,
+      closeOnEscape: false,
       size: 680,
       styles: {
         body: {
