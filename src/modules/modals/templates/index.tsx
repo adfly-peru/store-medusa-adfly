47ddc800e95f9170b29c231e9aa7d14165389d4f
juan4056 <juan.vargas@utec.edu.pe>
2024-01-24 08:17:54 -0500

    fix: only show modal when communication is false

--- src/modules/modals/templates/index.tsx
+++ src/modules/modals/templates/index.tsx
@@ -11,35 +11,36 @@ const CollaboratorModals: React.FC<{ children?: React.ReactNode }> = ({
   useEffect(() => {
     if (!collaborator) return;
     if (!collaborator.emailVerify || !collaborator.changePassword) return;
+    if ((collaborator.preferences?.prefercommunication ?? []).length === 0) {
+      const lastOpened = localStorage.getItem("surveyModalTime");
+      const now = new Date().getTime();
 
-    const lastOpened = localStorage.getItem("surveyModalTime");
-    const now = new Date().getTime();
+      // const fiveHoursInMs = 5 * 60 * 60 * 1000;
+      const fiveHoursInMs = 10 * 60 * 1000;
 
-    // const fiveHoursInMs = 5 * 60 * 60 * 1000;
-    const fiveHoursInMs = 10 * 60 * 1000;
+      if (lastOpened && now - parseInt(lastOpened) < fiveHoursInMs) {
+        return;
+      }
 
-    if (lastOpened && now - parseInt(lastOpened) < fiveHoursInMs) {
-      return;
+      localStorage.setItem("surveyModalTime", now.toString());
+
+      modals.open({
+        withCloseButton: false,
+        closeOnClickOutside: false,
+        closeOnEscape: false,
+        size: 680,
+        styles: {
+          body: {
+            paddingLeft: 0,
+            paddingRight: 0,
+          },
+          content: {
+            borderRadius: "0.5rem",
+          },
+        },
+        children: <SurveyModal />,
+      });
     }
-
-    localStorage.setItem("surveyModalTime", now.toString());
-
-    modals.open({
-      withCloseButton: false,
-      closeOnClickOutside: false,
-      closeOnEscape: false,
-      size: 680,
-      styles: {
-        body: {
-          paddingLeft: 0,
-          paddingRight: 0,
-        },
-        content: {
-          borderRadius: "0.5rem",
-        },
-      },
-      children: <SurveyModal />,
-    });
   }, [collaborator]);
 
   return <>{children}</>;
