d1bdb566fd9ef799906ea89d2cb81224cb604d37
juan4056 <juan.vargas@utec.edu.pe>
2024-02-13 23:31:33 -0500

    feat: implement fridays modal

--- src/modules/modals/templates/index.tsx
+++ src/modules/modals/templates/index.tsx
@@ -1,7 +1,17 @@
 import React, { useEffect } from "react";
 import { useAccount } from "@context/account-context";
 import { modals } from "@mantine/modals";
-import SurveyModal from "./components/survey-modal";
+import SurveyModal from "../components/survey-modal";
+import ValentinesModal from "../components/valentines/valentines-modal";
+import { Image } from "@mantine/core";
+
+function isTodayValentinesDay(): boolean {
+  const today = new Date();
+  const day = today.getDate();
+  const month = today.getMonth() + 1; // Los meses en JavaScript comienzan en 0
+
+  return day === 14 && month === 2;
+}
 
 const CollaboratorModals: React.FC<{ children?: React.ReactNode }> = ({
   children,
@@ -11,10 +21,8 @@ const CollaboratorModals: React.FC<{ children?: React.ReactNode }> = ({
   useEffect(() => {
     if (!collaborator) return;
     if (!collaborator.emailVerify || !collaborator.changePassword) return;
-    if (
-      (collaborator.preferences?.prefercommunication ?? []).length === 0 &&
-      typeof window !== "undefined"
-    ) {
+    if (!(typeof window !== undefined)) return;
+    if ((collaborator.preferences?.prefercommunication ?? []).length === 0) {
       const lastOpened = localStorage.getItem("surveyModalTime");
       const now = new Date().getTime();
 
@@ -41,6 +49,68 @@ const CollaboratorModals: React.FC<{ children?: React.ReactNode }> = ({
           },
         },
         children: <SurveyModal />,
+      });
+    }
+    // Valentines day modal
+    const valentinesmodal = localStorage.getItem("valentinesmodalviewed");
+    if (isTodayValentinesDay() && valentinesmodal === null) {
+      localStorage.setItem("valentinesmodalviewed", "viewed");
+      modals.open({
+        closeOnClickOutside: false,
+        closeOnEscape: false,
+        size: 880,
+        title: (
+          <Image
+            src="/fridays_coupon/Regalo.png"
+            alt="Regalo"
+            style={{
+              width: "200px",
+              height: "200px",
+            }}
+          ></Image>
+        ),
+        styles: {
+          body: {
+            padding: 0,
+          },
+          title: {
+            "@media (max-width: 62em)": {
+              position: "relative",
+              top: -15,
+              left: "25%",
+            },
+          },
+          close: {
+            marginRight: "1rem",
+            marginTop: 30,
+            color: "white",
+            "@media (max-width: 62em)": {
+              color: "red",
+            },
+          },
+          header: {
+            position: "relative",
+            backgroundColor: "transparent",
+            width: 800,
+            height: 110,
+            top: 60,
+            left: 80,
+            "@media (max-width: 62em)": {
+              position: "relative",
+              backgroundColor: "transparent",
+              width: "100%",
+              height: 110,
+              top: 60,
+              left: 0,
+            },
+          },
+          content: {
+            borderRadius: "2.5rem",
+            backgroundColor: "transparent",
+            boxShadow: "unset",
+          },
+        },
+        children: <ValentinesModal />,
       });
     }
   }, [collaborator]);
