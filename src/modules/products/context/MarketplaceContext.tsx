c17b6fd684856b717f492dddcd7dd6d011ca0242
Juan Vargas <juan.vargasg2@gmail.com>
2024-08-29 12:16:55 -0500

    feat: refetch marketplace on login with another account

--- src/modules/products/context/MarketplaceContext.tsx
+++ src/modules/products/context/MarketplaceContext.tsx
@@ -88,7 +88,8 @@ export const MarketplaceFiltersProvider = ({
     page,
     sort,
     fetch,
-    sessionData?.user,
+    sessionData?.user?.id,
+    sessionData?.user?.uuidbusiness,
     condition,
     rangePrices,
   ]);
